8ace2e73d1cdcc15fb8bf32a085a58c7a588b781
Carlos Garcia <carlosa.garcia@softtek.com>
2018-01-29 14:02:54 -0800

    Add node npm files

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// Following is required for Windows to avoid
+// 'absolute path required' error
+var path = require("path");
+var app_path = "./app/index.js";
+var build_path = "./build";
+
+module.exports = {
+    entry: [path.resolve(app_path)],
+    output: {
+        path: path.resolve(build_path),
+        filename: "bundle.js"
+    }
+}
